53f9b9e44ebe7a54c565f99cdd9bc00706a429ca
antonio <antoniorandria3342@gmail.com>
2026-01-10 19:15:51 +0300

    feat: add database connection properties and import script

---
+++ import.sh
@@ -0,0 +1,17 @@
+docker exec -it oracle-db bash
+
+sqlplus system/oracle
+
+CREATE USER atipik IDENTIFIED BY atipik;
+GRANT CONNECT, RESOURCE, DBA TO atipik;
+ALTER USER atipik DEFAULT TABLESPACE USERS;
+ALTER USER atipik TEMPORARY TABLESPACE TEMP;
+exit
+
+
+
+
+
+docker cp /home/antonio/Bureau/S5/mr-tahina/apj-location-meuble/atipik0601.dmp oracle-db:/home/oracle/
+docker exec -it oracle-db bash
+imp atipik/atipik file=/home/oracle/atipik0601.dmp full=yes ignore=yes
